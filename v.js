812b33a47174c7ad5bb20ee725898976a773d9d0
Matiasxds <xdsmatias@gmail.com>
2022-07-29 21:20:06 -0300

    git pull

---
+++ v.js
@@ -0,0 +1,11 @@
+// Noindex: Tells a search engine not to index a page.
+
+// Index: Tells a search engine to index a page. Note that you don’t need to add this meta tag; it’s the default.
+
+// Follow: Even if the page isn’t indexed, the crawler should follow all the links on a page and pass equity to the linked pages.
+
+// Nofollow: Tells a crawler not to follow any links on a page or pass along any link equity.
+
+// Noimageindex: Tells a crawler not to index any images on a page.
+
+// None: Equivalent to using both the noindex and nofollow tags simultaneously.
